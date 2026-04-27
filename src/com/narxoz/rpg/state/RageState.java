e27efd512e861d3d0894295d4933996bba1376fe
nurbolatb0903-arch <nurbolat.b0903@icloud.com>
2026-04-27 07:47:00 +0500

    add RageState for low HP berserker mode

--- src/com/narxoz/rpg/state/RageState.java
+++ src/com/narxoz/rpg/state/RageState.java
@@ -1,4 +1,32 @@
 package com.narxoz.rpg.state;
 
-public class RageState {
+import com.narxoz.rpg.combatant.Hero;
+
+public class RageState implements HeroState {
+    private int turnsActive = 0;
+    private static final int MAX_TURNS = 3;
+
+    @Override
+    public String getStateName() {
+        return "Rage";
+    }
+
+    @Override
+    public int modifyDamage(int damage) {
+        return damage * 2;
+    }
+
+    @Override
+    public int modifyReceivedDamage(int damage) {
+        return damage;
+    }
+
+    @Override
+    public HeroState transition(Hero hero) {
+        turnsActive++;
+        if (turnsActive >= MAX_TURNS || hero.getHp() > hero.getMaxHp() * 0.3) {
+            return new NormalState();
+        }
+        return this;
+    }
 }
