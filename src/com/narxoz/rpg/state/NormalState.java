de979eb649441da572f08b858f500c7433c8e539
nurbolatb0903-arch <nurbolat.b0903@icloud.com>
2026-04-27 07:46:22 +0500

    add NormalState as default hero state

--- src/com/narxoz/rpg/state/NormalState.java
+++ src/com/narxoz/rpg/state/NormalState.java
@@ -1,4 +1,34 @@
 package com.narxoz.rpg.state;
 
-public class NormalState {
+import com.narxoz.rpg.combatant.Hero;
+
+public class NormalState implements HeroState {
+    @Override
+    public String getStateName() {
+        return "Normal";
+    }
+
+    @Override
+    public int modifyDamage(int damage) {
+        return damage;
+    }
+
+    @Override
+    public int modifyReceivedDamage(int damage) {
+        return damage;
+    }
+
+    @Override
+    public HeroState transition(Hero hero) {
+        if (hero.getHp() <= hero.getMaxHp() * 0.3) {
+            return new RageState();
+        }
+        if (hero.isStunned()) {
+            return new StunnedState();
+        }
+        if (hero.isPoisoned()) {
+            return new PoisonedState();
+        }
+        return this;
+    }
 }
