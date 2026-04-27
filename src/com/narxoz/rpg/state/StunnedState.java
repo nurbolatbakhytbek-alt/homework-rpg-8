9f5c4151536bd63e82ed10386fa10b297c1961e2
nurbolatb0903-arch <nurbolat.b0903@icloud.com>
2026-04-27 07:48:11 +0500

    add StunnedState for crowd control effect

--- src/com/narxoz/rpg/state/StunnedState.java
+++ src/com/narxoz/rpg/state/StunnedState.java
@@ -1,4 +1,33 @@
 package com.narxoz.rpg.state;
 
-public class StunnedState {
+import com.narxoz.rpg.combatant.Hero;
+
+public class StunnedState implements HeroState {
+    private int turnsActive = 0;
+    private static final int MAX_TURNS = 2;
+
+    @Override
+    public String getStateName() {
+        return "Stunned";
+    }
+
+    @Override
+    public int modifyDamage(int damage) {
+        return 0;
+    }
+
+    @Override
+    public int modifyReceivedDamage(int damage) {
+        return damage;
+    }
+
+    @Override
+    public HeroState transition(Hero hero) {
+        turnsActive++;
+        if (turnsActive >= MAX_TURNS) {
+            hero.getName(false);
+            return new NormalState();
+        }
+        return this;
+    }
 }
